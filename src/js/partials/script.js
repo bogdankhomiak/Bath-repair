f0d0ae6af180fe8548353c59ce13bda1d7cc9cb8
bogdankhomiak <peculiarsev@gmail.com>
2017-09-26 15:23:29 +0300

    rates

--- src/js/partials/script.js
+++ src/js/partials/script.js
@@ -1,12 +1,21 @@
 'use strict';
-$("#rangeSlider").ionRangeSlider({
-    type: "single",
-    grid: true,
-    min: 1,
-    max: 10,
-    from: 5.5,
-    grid_num: 9,
-    step: 0.5,
-    hide_min_max: true,
-    postfix: " м<sup>2</sup>"
+$(function () {
+    $("#rangeSlider").ionRangeSlider({
+        type: "single",
+        grid: true,
+        min: 1,
+        max: 10,
+        from: 5.5,
+        grid_num: 9,
+        step: 0.5,
+        hide_min_max: true,
+        postfix: " м<sup>2</sup>"
+    });
+});
+$(function () {
+    $('.tabs__items').on('click', 'li:not(.active)', function () {
+        $(this)
+            .addClass('tabs__item_active').siblings().removeClass('tabs__item_active')
+            .closest('.tabs').find('.tabs__content').removeClass('tabs__content_active').eq($(this).index()).addClass('tabs__content_active');
+    });
 });
